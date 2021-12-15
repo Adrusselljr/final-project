13b75455d81cd0375afbd48587f2857141f4b34c
Aaron Russell Jr <adrusselljr@Aarons-MacBook-Pro.local>
2021-12-15 15:25:09 -0500

    modified js

--- main.js
+++ main.js
@@ -21,32 +21,66 @@ const cardFlip = new Audio('./sounds/cardFlip.ogg')
 const cardShuffle = new Audio('./sounds/cardShuffle.wav')
 const correct = new Audio('./sounds/correct.wav')
 
-// Create board based on difficulty
+// Get card values
 if(difficulty === "easy") {
-    for(let i = 0; i < 8; i++) {
-        const layout = $(`<div class="card"><img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back"></div>`)
-        board.append(layout)
-    }
+    cardNum = [1, 1, 2, 2, 3, 3, 4, 4]
 }
 
 if(difficulty === "medium") {
-    for(let i = 0; i < 12; i++) {
-        const layout = $(`<div class="card"><img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back"></div>`)
-        board.append(layout)
-    }
+    cardNum = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
 }
 
 if(difficulty === "hard") {
-    for(let i = 0; i < 16; i++) {
-        const layout = $(`<div class="card"><img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back"></div>`)
-        board.append(layout)
-    }
+    cardNum = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 }
 
+cardShuffle.play()
+
+// Create board based on difficulty
+setTimeout(() => {
+
+    if(difficulty === "easy") {
+        for(let i = 0; i < 8; i++) {
+            const layout = $(`
+                <div class="card">
+                    <img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back">
+                </div>
+            `)
+            board.append(layout)
+        }
+    }
+    
+    if(difficulty === "medium") {
+        for(let i = 0; i < 12; i++) {
+            const layout = $(`
+                <div class="card">
+                    <img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back">
+                </div>
+            `)
+            board.append(layout)
+        }
+    }
+    
+    if(difficulty === "hard") {
+        for(let i = 0; i < 16; i++) {
+            const layout = $(`
+                <div class="card">
+                    <img id="${i}" class="cardImg" src="./images/cardBack.jpeg" alt="Card Back">
+                </div>
+            `)
+            board.append(layout)
+        }
+    }
+
+}, 250)
+
+cardNum = _.shuffle(cardNum)
+
 // Reset board
-const card = $(".cardImg")
 
 const reset = () => {
+    
+    const card = $(".cardImg")
 
     const winner = $("<h3 id='winMessage'>YOU WON!</h3>")
     message.append(winner)
@@ -63,61 +97,45 @@ const reset = () => {
 
 }
 
-// Get card values
-if(difficulty === "easy") {
-    cardNum = [1, 1, 2, 2, 3, 3, 4, 4]
-}
-
-if(difficulty === "medium") {
-    cardNum = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
-}
-
-if(difficulty === "hard") {
-    cardNum = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
-}
-
-cardNum = _.shuffle(cardNum)
-cardShuffle.play()
-
-// Set up click listener
-const oneCard = $(".card")
-
+// Setup onClick for board
 board.on('click', e => {
 
     const index = $(e.target).attr("id")
 
+    cardFlip.play()
+
     // Set clicked card image
     if(cardNum[index] === 1) {
         $(e.target).attr("src", "./images/ace.jpg")
-        cardFlip.play()
+        $(e.target).attr("alt", "Ace of Clubs")
     }
     if(cardNum[index] === 2) {
         $(e.target).attr("src", "./images/king.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "King of Hearts")
     }
     if(cardNum[index] === 3) {
         $(e.target).attr("src", "./images/queen.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Queen of Diamonds")
     }
     if(cardNum[index] === 4) {
         $(e.target).attr("src", "./images/jack.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Jack of Spades")
     }
     if(cardNum[index] === 5) {
         $(e.target).attr("src", "./images/ten.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Ten of Spades")
     }
     if(cardNum[index] === 6) {
         $(e.target).attr("src", "./images/nine.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Nine of Hearts")
     }
     if(cardNum[index] === 7) {
         $(e.target).attr("src", "./images/eight.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Eight of Spades")
     }
     if(cardNum[index] === 8) {
         $(e.target).attr("src", "./images/seven.png")
-        cardFlip.play()
+        $(e.target).attr("alt", "Seven of Diamonds")
     }
 
     // Check if cards match or not
@@ -156,7 +174,9 @@ board.on('click', e => {
             else {
 
                 firstClick.attr("src", "./images/cardBack.jpeg")
+                firstClick.attr("alt", "Card Back")
                 $(e.target).attr("src", "./images/cardBack.jpeg")
+                $(e.target).attr("alt", "Card Back")
                 cardFlip.play()
                 console.log("dont match")
 
