f64cef608da5d98be37a7890e76f804202fed136
Aaron Russell Jr <adrusselljr@Aarons-MacBook-Pro.local>
2021-12-20 11:13:02 -0500

    modified js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Set Query Params
 const urlSearchParams = new URLSearchParams(window.location.search)
 const params = Object.fromEntries(urlSearchParams.entries())
-
 const difficulty = params.difficulty
 
 // Query elements
